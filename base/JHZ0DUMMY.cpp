c5458300f2ab0bade623d5b0fcdb114be3ab23be
Frooze-ai <67207166+Frooze-ai@users.noreply.github.com>
2024-01-07 12:26:23 +0300

    Add dummy.cpp via web-upl

---
+++ base/JHZ0DUMMY.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+std::string sifrele(const std::string& metin, int anahtar) {
+    std::string sifreliMetin = metin;
+    for (char& karakter : sifreliMetin) {
+        karakter = karakter + anahtar;
+    }
+    return sifreliMetin;
+}
+
+std::string coz(const std::string& sifreliMetin, int anahtar) {
+    return sifrele(sifreliMetin, -anahtar);
+}
+
+int main() {
+    std::string metin;
+    int anahtar;
+
+    // Kullanýcýdan metni ve þifreleme anahtarýný al
+    std::cout << "Sifrelenecek metni girin: ";
+    std::getline(std::cin, metin);
+
+    std::cout << "Key'i girin(rastgele sayilar, harfler ve semboller) ";
+    std::cin >> anahtar;
+
+    // Þifreleme
+    std::string sifreliMetin = sifrele(metin, anahtar);
+    std::cout << "Sifreli metin: " << sifreliMetin << std::endl;
+
+    // Þifreyi çözme
+    std::string orijinalMetin = coz(sifreliMetin, anahtar);
+    std::cout << "Orijinal metin: " << orijinalMetin << std::endl;
+
+    return 0;
+}
